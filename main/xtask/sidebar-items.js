2758506b372acef7ae7a52f7131bd0d9412791e0
kl-botsu <botsu@khonsulabs.com>
2022-01-31 15:30:58 +0000

    Deploying to gh-pages from @ khonsulabs/bonsaidb@5f2d7655b0456c27d719a129e8fb592361cc1a41 🚀

--- main/xtask/sidebar-items.js
+++ main/xtask/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Commands",""],["Config",""]],"fn":[["all_tests",""],["generate_test_matrix_output",""],["main",""],["run_all_tests",""]],"struct":[["TestMatrix",""],["TestSuite",""]]});
+initSidebarItems({"enum":[["Commands",""],["Config",""]],"fn":[["all_tests",""],["generate_test_matrix_output",""],["last_succeeded_index",""],["main",""],["run_all_tests",""],["set_last_succeeded_index",""]],"struct":[["TestMatrix",""],["TestSuite",""]]});
