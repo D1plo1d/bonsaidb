e5ba3b1e5aa64af149f60782f112c45eeb05edb8
kl-botsu <botsu@khonsulabs.com>
2022-02-06 18:19:27 +0000

    Deploying to gh-pages from @ khonsulabs/bonsaidb@f0001ec57a64fe9b527a5f767718407a5487372b 🚀

--- main/docs/bonsaidb_macros/sidebar-items.js
+++ main/docs/bonsaidb_macros/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"derive":[["Action","Derives the `bonsaidb_core::permissions::Action` trait."]]});
+initSidebarItems({"derive":[["Collection","Derives the `bonsaidb::core::schema::Collection` trait. `#[collection(authority = \"Authority\", name = \"Name\", views = [a, b, c])]`"],["View","Derives the `bonsaidb::core::schema::View` trait. `#[view(collection=CollectionType, key=KeyType, value=ValueType, name = \"by-name\")]` `name` and `value` are optional"]]});
