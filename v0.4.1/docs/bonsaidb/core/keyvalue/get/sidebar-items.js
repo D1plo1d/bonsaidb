dce4bac1501a2b03dab5fcec49dd55d04238be07
kl-botsu <botsu@khonsulabs.com>
2022-04-05 00:36:27 +0000

    Deploying to gh-pages from @ khonsulabs/bonsaidb@500b0a04b0cfa2f9b44c8d5c68ff17a90aa8afe4 🚀

---
+++ v0.4.1/docs/bonsaidb/core/keyvalue/get/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["AsyncBuilder","Builder for a [`Command::Get`] key-value operation. Queries the value when awaited."],["Builder","Builder for a [`Command::Get`] key-value operation."]]});
